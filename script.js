4538d7d01ff6d01f65aeb8e905f8c8f8bb02f160
Amgalanbaatar1 <amgaabra08@gmail.com>
2023-12-26 20:39:52 +0800

    To Do GG

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
-function myFunction() {
-  document.getElementById("demo").innerHTML = "Hello World";
+function showModal() {
+  const modal = document.getElementById("createModal");
+
+  modal.style.display = "flex";
+  console.log(modal);
 }
+
+function hideModal() {
+  const modal = document.getElementById("createModal");
+  modal.style.display = "none";
+}
+
+function displayDate() {
+  let date = new Date();
+  date = date.toString().split(" ");
+  document.querySelector("#date").innerHTML = date[1] + " " + date[2] + " " + date[3];
+}
+
+window.onload = function () {
+  displayDate();
+};
