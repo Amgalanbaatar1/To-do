8cd1b9538748a12419af9e9f45aa1748e1a78a9d
Amgalanbaatar1 <amgaabra08@gmail.com>
2024-01-08 21:02:49 +0800

    edith.tasks.duusagui

--- script.js
+++ script.js
@@ -6,9 +6,126 @@ function displayDate() {
 window.onload = function () {
   displayDate();
 };
+const tasks = [
+  {
+    id: 1,
+    title: "Title 1",
+    description: "description",
+    priority: "High",
+    status: "todo",
+    position: 6,
+  },
+
+  {
+    id: -1,
+    title: "Title 2",
+    description: "description",
+    priority: "High",
+    status: "inprogress",
+    position: 2,
+  },
+  {
+    id: -2,
+    title: "Title 3",
+    description: "description",
+    priority: "Medium",
+    status: "inprogress",
+    position: 4,
+  },
+  {
+    id: -3,
+    title: "Title 4",
+    description: "description",
+    priority: "Low",
+    status: "todo",
+    position: 3,
+  },
+  {
+    id: -4,
+    title: "Title 5",
+    description: "description",
+    priority: "Low",
+    status: "done",
+    position: 1,
+  },
+  {
+    id: -5,
+    title: "Title 5",
+    description: "description",
+    priority: "Medium",
+    status: "stuck",
+    position: 5,
+  },
+];
 
 let currentTarget = "todoTask";
 let generatedId = 0;
+let editingIndex;
+
+function renderTasks() {
+  const todoTaskElments = document.getElementById("todoTasks");
+  const progressTaskElments = document.getElementById("progressTasks");
+  const stuckTaskElments = document.getElementById("stuckTasks");
+  const doneTaskElments = document.getElementById("doneTasks");
+
+  let todoResult = "";
+  let inprogressResult = "";
+  let stuckResult = "";
+  let doneResult = "";
+
+  tasks.sort((a1, a2) => {
+    const priorityNumbers = {
+      low: 1,
+      medium: 2,
+      high: 3,
+    };
+
+    if (priorityNumbers[a1.priority] > priorityNumbers[a2.priority]) {
+      return 1;
+    } else {
+      return -1;
+    }
+  });
+
+  for (let k = 0; k < tasks.length; k++) {
+    const task = tasks[k];
+    if ((task.title === "", task.des === "")) {
+      alert("Ta utga oruulna uu?");
+    } else {
+      generatedId++;
+      const taskHTML = `
+      <div class="addedList" id="task-${generatedId}">
+                <i style="position: absolute; top: 9px; left: 15px;font-size: 25px" class="fa-regular fa-circle-check"></i>
+                <div class="titleDes">
+                  <h2>${task.title}</h2>
+                  <p>${task.des}</p>
+                  <button style="border-radius: 5px;">${task.priority}</button>
+                </div>
+                <i onclick="removeBtn('task-${generatedId}')" style="position: absolute; top: 9px; right: 28px;font-size: 25px" class="fa-regular fa-circle-xmark"></i>
+                <i onclick="editBtn()"style="position: absolute; top: 44px; right: 28px;font-size: 25px" class="fa-solid fa-pen-to-square"></i>
+              </div>
+    
+      `;
+      switch (task.status) {
+        case "todo":
+          todoResult += taskHTML;
+          break;
+        case "inprogress":
+          inprogressResult += taskHTML;
+          break;
+        case "stuck":
+          stuckResult += taskHTML;
+          break;
+        case "done":
+          doneResult += taskHTML;
+      }
+    }
+  }
+  todoTaskElments.innerHTML = todoResult;
+  progressTaskElments.innerHTML = inprogressResult;
+  stuckTaskElments.innerHTML = stuckResult;
+  doneTaskElments.innerHTML = doneResult;
+}
 
 function resetForm() {
   document.getElementById("titleInput").value = "";
@@ -32,28 +149,27 @@ function addTask() {
   const statusTasks = document.getElementById("status").value;
   const priority = document.getElementById("taskPriority").value;
 
-  generatedId++;
-  const taskHTML = `
-  <div class="addedList" id="task-${generatedId}">
-            <i style="position: absolute; top: 9px; left: 15px;font-size: 25px" class="fa-regular fa-circle-check"></i>
-            <div class="titleDes">
-              <h2>${title}</h2>
-              <p>${des}</p>
-              <button style="border-radius: 5px;">${priority}</button>
-            </div>
-            <i onclick="removeBtn('task-${generatedId}')" style="position: absolute; top: 9px; right: 28px;font-size: 25px" class="fa-regular fa-circle-xmark"></i>
-            <i style="position: absolute; top: 44px; right: 28px;font-size: 25px" class="fa-solid fa-pen-to-square"></i>
-          </div>
-
-  `;
-  const todoElment = document.getElementById(currentTarget);
-  todoElment.innerHTML = todoElment.innerHTML + taskHTML;
-
+  tasks.push({
+    id: generatedId++,
+    title: title,
+    description: des,
+    statusTasks: statusTasks,
+    priority: priority,
+  });
+  renderTasks();
   hideModal();
-
   resetForm();
 }
 
 function removeBtn(taskId) {
   document.getElementById(taskId).remove();
 }
+
+function editBtn(index) {
+  document.getElementById("titleInput").value = tasks[index].title;
+  document.getElementById("description").value = tasks[index].description;
+  document.getElementById("status").value = tasks[index].status;
+  document.getElementById("taskPriority").value = tasks[index].priority;
+}
+
+renderTasks();
